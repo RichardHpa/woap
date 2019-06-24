0f26756ec7f68313a22c67786597b413b1cbc41e
Richard Hpa <richard.hpa@hotmail.com>
2019-06-24 14:58:17 +1200

    installed app and starting to develop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './woap19-logo.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Wellington on a Plate
+        </h2>
+        <p>A application made for the Wellington on a Plate food festival which will help you plan which places you go to during the festival.</p>
+        <p>This will be an unoffical app and not created by the offical WOAP organisers.</p>
       </header>
     </div>
   );
