f0343a2795646dab92bc80ea9fa567e3b32597b2
RichardHpa <richard.hpa@hotmail.com>
2019-07-02 15:26:25 +1200

    more information gets rendred out

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
 import axios from 'axios'
 import './Map.scss';
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -16,6 +16,8 @@ export class MapContainer extends Component {
             currentView: []
         }
         this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.getMarkers = this.getMarkers.bind(this);
+        this.onMapClicked = this.onMapClicked.bind(this);
     }
 
     componentDidMount () {
@@ -27,64 +29,17 @@ export class MapContainer extends Component {
                     allLocations.push(response.data.venues[i]);
                 }
             }
-            // console.log(allLocations);
             this.setState({
                 locations: allLocations
-            })
+            });
+            this.getMarkers();
         })
     }
 
     componentDidUpdate(prevProps) {
-        // this.setState({
-        //     markers: []
-        // })
       if (this.props.currentView !== prevProps.currentView) {
-          const { locations } = this.state;
-          var newView = this.props.currentView;
-          var eventID = 0;
-          switch(newView){
-              case 'event':
-                eventID = 29;
-              break;
-              case 'burger':
-                eventID = 30;
-              break;
-              case 'dine':
-                  eventID = 31;
-              break;
-              case 'cocktail':
-                eventID = 32;
-              break;
-              default:
-
-              break;
-          }
-
-          var eventsToShow = [];
-          var iterationNum = 1
-          for (var i = 0; i < locations.length; i++) {
-              var venue = locations[i];
-              for (var j = 0; j < venue.Event.length; j++) {
-                  var singleEvent = venue.Event[j];
-                  if(singleEvent['event_type_id'] == eventID){
-                      var latLng = venue.Venue.ll.split(",");
-                      eventsToShow.push({
-                          id: iterationNum,
-                          venueDetails: venue.Venue,
-                          eventDetails: singleEvent,
-                          lat: parseFloat(latLng[0]),
-                          lng: parseFloat(latLng[1])
-                      });
-                      iterationNum++;
-                  }
-                }
-            }
-            this.setState({
-                markers: eventsToShow,
-                currentType: newView
-            })
+          this.getMarkers();
         }
-
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -92,23 +47,66 @@ export class MapContainer extends Component {
     }
 
     renderMarkers() {
-      return this.state.markers.map((marker, i) => {
-        return <Marker
-          key={i}
-          markerID={marker.id}
-          title={marker.title}
-          name={marker.title}
-          animation={this.props.google.maps.Animation.DROP}
-          position={{lat: marker.lat, lng: marker.lng}}
-          onClick={this.onMarkerClick}
-          />
-      })
+        const {currentType} = this.state;
+        return this.state.markers.map((marker, i) => {
+            return <Marker
+                key={i}
+                markerID={marker.id}
+                title={marker.title}
+                name={marker.title}
+                animation={this.props.google.maps.Animation.DROP}
+                position={{lat: marker.lat, lng: marker.lng}}
+                onClick={this.onMarkerClick}
+                icon={{ url: '/markers/'+currentType+'Marker.png' }}
+            />
+        })
     }
 
-    boundsChanged(bounds){
-        console.log('here');
+    getMarkers(){
+        const { locations } = this.state;
+        var newView = this.props.currentView;
+        var eventID = 0;
+        switch(newView){
+            case 'events':
+              eventID = 29;
+            break;
+            case 'burger':
+              eventID = 30;
+            break;
+            case 'dine':
+                eventID = 31;
+            break;
+            case 'cocktail':
+              eventID = 32;
+            break;
+            default:
 
-        // console.log(this.map.getBounds().getNorthEast());
+            break;
+        }
+
+        var eventsToShow = [];
+        var iterationNum = 1
+        for (var i = 0; i < locations.length; i++) {
+            var venue = locations[i];
+            for (var j = 0; j < venue.Event.length; j++) {
+                var singleEvent = venue.Event[j];
+                if(singleEvent['event_type_id'] == eventID){
+                    var latLng = venue.Venue.ll.split(",");
+                    eventsToShow.push({
+                        id: iterationNum,
+                        venueDetails: venue.Venue,
+                        eventDetails: singleEvent,
+                        lat: parseFloat(latLng[0]),
+                        lng: parseFloat(latLng[1])
+                    });
+                    iterationNum++;
+                }
+              }
+          }
+          this.setState({
+              markers: eventsToShow,
+              currentType: newView
+          })
     }
 
 
@@ -118,12 +116,22 @@ export class MapContainer extends Component {
         for (var i = 0; i < markers.length; i++) {
             if(markers[i].id === markerID){
                 this.props.selectedMarker(markers[i]);
+                this.setState({
+                  selectedPlace: props,
+                  activeMarker: marker,
+                  showingInfoWindow: true
+                });
                 break;
             }
         }
     }
 
+    onMapClicked(){
+        this.props.mapClick();
+    }
+
     render() {
+
       return (
           <Map
               google={this.props.google}
@@ -132,19 +140,19 @@ export class MapContainer extends Component {
                  lng: 174.7762
                }}
                zoom={12}
-               onDragend={this.boundsChanged}
+               minZoom = {10}
+               minZooom={9}
                styles={styles}
+               onClick={this.onMapClicked}
 
           >
           {
               this.renderMarkers()
           }
-
           </Map>
       );
     }
   }
-
 
 
 export default GoogleApiWrapper({
@@ -176,8 +184,3 @@ export default GoogleApiWrapper({
 //       })
 //   }
 // </div>
-
-// var u = r.eventTypes && 3 === r.eventTypes.length;
-// (u || -1 !== r.eventTypes.indexOf("dine")) && f(a, "Event.what_dietaries_are_or_could_be_catered_to_in_this_menu_from_all_dishes", r.dietary),
-// (u || -1 !== r.eventTypes.indexOf("burger")) && f(a, "Event.burger_dietary_requirements", r.dietary),
-// (u || -1 !== r.eventTypes.indexOf("cocktail")) && f(a, "Event.cocktail_dietary_requirements", r.dietary)
